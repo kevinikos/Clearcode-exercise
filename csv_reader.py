a62898d57fe786bef5ef1bc5223295fb388274c7
mrpapaj <kevin.nikolas123@gmail.com>
2019-02-03 23:49:03 +0100

    added functions

--- csv_reader.py
+++ csv_reader.py
@@ -1,18 +1,27 @@
 from datetime import datetime
+import pycountry
 import csv
 
-filename = 'clearcode-csv.csv'
-with open(filename, encoding="utf8") as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=';')
-    Data = []
-    for row in readCSV:
-        Data.append(row[0:3])
+def ReadFile():
+    filename = 'clearcode-csv.csv'
+    with open(filename, encoding="utf8") as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=';')
+        Data = []
+        for row in readCSV:
+            Data.append(row[0:])
+    return Data
 
-for line in Data:
-    try:
-        DateFormat = datetime.strptime(line[0], '%m/%d/%Y').strftime('%Y-%m-%d')
-        line[0] = DateFormat
-    except ValueError as e:
-        print(e)
+def ModifyDate():
+    Data = ReadFile()
+    for line in Data:
+        try:
+            DateFormat = datetime.strptime(line[0], '%m/%d/%Y').strftime('%Y-%m-%d')
+            line[0] = DateFormat
+        except ValueError as e:
+            print(e)
+    return Data
 
-print(Data)
+Data = ModifyDate()
+
+# germany = pycountry.countries.get(name='Mandiana')
+# print(germany)
