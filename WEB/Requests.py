a5fc9e0ccfbc859e1f11eff64b96816a6d9d7297
mrpapaj <kevin.nikolas123@gmail.com>
2019-02-05 13:18:37 +0100

    parse with bs4 library, added dictionary with url, title and links

--- WEB/Requests.py
+++ WEB/Requests.py
@@ -1,6 +1,7 @@
 import urllib.request
 import re
-import time
+import requests
+from bs4 import BeautifulSoup
 
 domain = "http://127.0.0.1:8000"
 urls = ["", "/site.html", "/example.html", "/site/subsite.html", "/site/other_site.html"]
@@ -12,13 +13,32 @@ for x in range(0, len(urls)):
 pattern = b'<title>(.+?)</title>'
 result = re.compile(pattern)
 
+data = {}
+titles = {}
+links = {}
+
 for url in urls:
+	data[url] = titles, links
+	titles["title"] = []
+	links["link"] = []
+
+	# titles:
 	htmlsource = urllib.request.urlopen(url)
 	htmltext = htmlsource.read()
-	titles = re.findall(result, htmltext)
-	print(titles)
+	title = re.findall(result, htmltext)
+	titles["title"].append(title)
+	print("TITLE:", title)
+	# links:
+	html_page = requests.get(url).text
+	soup = BeautifulSoup(html_page, "lxml")
+	for link in soup.findAll('a'):
+		print(link.get('href'))
+		links["link"].append(link.get('href'))
 
-# with urllib.request.urlopen(url) as response:
-# 	encoding = response.info().get_param('charset', 'utf8')
-# 	html = response.read().decode(encoding)
-# 	print(html)
+print(data)
+# for url in urls:
+# 	html_page = requests.get(url).text
+# 	soup = BeautifulSoup(html_page, "lxml")
+# 	for link in soup.findAll('a'):
+# 		print(link.get('href'))
+
