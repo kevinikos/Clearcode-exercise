441c5effc8e5f6b5a8bdac102e9fc6ab69962fe2
mrpapaj <kevin.nikolas123@gmail.com>
2019-02-05 13:56:44 +0100

    convert to json

--- WEB/Requests.py
+++ WEB/Requests.py
@@ -1,7 +1,6 @@
-import urllib.request
-import re
-import requests
 from bs4 import BeautifulSoup
+import urllib.request
+import json
 
 domain = "http://127.0.0.1:8000"
 urls = ["", "/site.html", "/example.html", "/site/subsite.html", "/site/other_site.html"]
@@ -10,35 +9,24 @@ for x in range(0, len(urls)):
 	url = domain + urls[x]
 	urls[x] = url
 
-pattern = b'<title>(.+?)</title>'
-result = re.compile(pattern)
-
 data = {}
-titles = {}
-links = {}
-
 for url in urls:
-	data[url] = titles, links
-	titles["title"] = []
-	links["link"] = []
 
-	# titles:
-	htmlsource = urllib.request.urlopen(url)
-	htmltext = htmlsource.read()
-	title = re.findall(result, htmltext)
-	titles["title"].append(title)
-	print("TITLE:", title)
-	# links:
-	html_page = requests.get(url).text
-	soup = BeautifulSoup(html_page, "lxml")
-	for link in soup.findAll('a'):
-		print(link.get('href'))
-		links["link"].append(link.get('href'))
+	title = {}
+	links = {}
+	links["links"] = []
+	title["title"] = []
+	data[url] = title, links
 
-print(data)
-# for url in urls:
-# 	html_page = requests.get(url).text
-# 	soup = BeautifulSoup(html_page, "lxml")
-# 	for link in soup.findAll('a'):
-# 		print(link.get('href'))
+	page = urllib.request.urlopen(url)
+	html = BeautifulSoup(page.read(), "html.parser")
+	title["title"] = html.title.string
+	# print(html.title.string)
+
+	for link in html.findAll('a'):
+		links["links"].append(link.get('href'))
+		# print(link.get('href'))
+
+print(json.dumps(data, indent=2, sort_keys=True))
+
 
