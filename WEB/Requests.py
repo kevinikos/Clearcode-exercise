af5cb9b81046aa3b549931028ad4a769bfe859d8
mrpapaj <kevin.nikolas123@gmail.com>
2019-02-05 15:03:08 +0100

    address has been changed 0.0.0.0 -> 127.0.0.1

--- WEB/Requests.py
+++ WEB/Requests.py
@@ -3,7 +3,8 @@ import urllib.request
 import json
 
 domain = "http://127.0.0.1:8000"
-urls = ["", "/site.html", "/example.html", "/site/subsite.html", "/site/other_site.html"]
+
+urls = ["", "/site.html", "/example.html", "/site/subsite.html", "/site/other_site.html"] # available links
 
 for x in range(0, len(urls)):
 	url = domain + urls[x]
@@ -24,8 +25,20 @@ for url in urls:
 	# print(html.title.string)
 
 	for link in html.findAll('a'):
-		links["links"].append(link.get('href'))
+		extract = link.get('href')
 		# print(link.get('href'))
+
+		if "http://0.0.0.0:8000" in extract: # changed to local address 127.0.0.1
+			extract = extract.replace("http://0.0.0.0:8000", domain)
+			links["links"].append(extract)
+
+		elif "http" not in extract:
+			extract = domain + extract
+			links["links"].append(extract)
+
+		# if you want to display links from other website
+		# else:
+		# 	links["links"].append(x)
 
 print(json.dumps(data, indent=2, sort_keys=True))
 
